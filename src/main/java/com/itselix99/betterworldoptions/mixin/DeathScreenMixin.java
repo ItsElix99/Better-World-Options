09a0fdd6a72e93d9fadc1804e178e795c7cb84b8
Elix <elixgamerbr99@gmail.com>
2025-05-14 19:35:05 -0300

    WorldProperties interface name has been changed to BWOProperties

--- src/main/java/com/itselix99/betterworldoptions/mixin/DeathScreenMixin.java
+++ src/main/java/com/itselix99/betterworldoptions/mixin/DeathScreenMixin.java
@@ -1,6 +1,6 @@
 package com.itselix99.betterworldoptions.mixin;
 
-import com.itselix99.betterworldoptions.interfaces.WorldProperties;
+import com.itselix99.betterworldoptions.interfaces.BWOProperties;
 import com.itselix99.betterworldoptions.interfaces.GetDirectoryName;
 import com.itselix99.betterworldoptions.world.WorldSettings;
 import com.itselix99.betterworldoptions.world.WorldTypeList;
