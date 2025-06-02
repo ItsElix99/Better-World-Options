9ed561a967222a64db556fd131bc4799babde4b7
Elix <elixgamerbr99@gmail.com>
2025-06-01 21:03:52 -0300

    Fixed delay when applying alpha cobblestone texture

--- src/main/java/com/itselix99/betterworldoptions/mixin/blocks/BlockMixin.java
+++ src/main/java/com/itselix99/betterworldoptions/mixin/blocks/BlockMixin.java
@@ -3,37 +3,28 @@ package com.itselix99.betterworldoptions.mixin.blocks;
 import com.itselix99.betterworldoptions.events.TextureListener;
 import com.itselix99.betterworldoptions.world.WorldSettings;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Block.class)
-public class BlockMixin {
-    @Shadow public int textureId;
+public abstract class BlockMixin {
     @Shadow @Final @Mutable public final int id;
 
     public BlockMixin(int id) {
         this.id = id;
     }
 
-    @Environment(EnvType.CLIENT)
-    @ModifyReturnValue(method = "getTextureId", at = @At("RETURN"))
-    private int getTexture(int original) {
+    @ModifyReturnValue(method = "getTexture*", at = @At("RETURN"))
+    public int getTexture(int original, int side) {
         if (!WorldSettings.isBetaFeatures) {
             if (this.id == 4) {
-                this.textureId = TextureListener.alphaCobblestone;
-                return this.textureId;
+                return TextureListener.alphaCobblestone;
             } else {
                 return original;
             }
         } else {
-            if (this.id == 4) {
-                this.textureId = 16;
-                return this.textureId;
-            }
+            return original;
         }
-        return original;
     }
 }
