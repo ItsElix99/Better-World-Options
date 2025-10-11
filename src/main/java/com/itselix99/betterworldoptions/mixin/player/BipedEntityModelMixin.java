2ee35feb7d6a1894e7c961c0bb330f6d53782c4a
Elix <elixgamerbr99@gmail.com>
2025-10-11 16:17:04 -0300

    Speed up Infdev and Classic walking animations

--- src/main/java/com/itselix99/betterworldoptions/mixin/player/BipedEntityModelMixin.java
+++ src/main/java/com/itselix99/betterworldoptions/mixin/player/BipedEntityModelMixin.java
@@ -10,7 +10,9 @@ import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BipedEntityModel.class)
@@ -20,6 +22,30 @@ public class BipedEntityModelMixin extends EntityModel {
     @Shadow public ModelPart rightArm;
     @Shadow public ModelPart leftArm;
 
+    @ModifyConstant(
+            method = "setAngles",
+            constant = @Constant(floatValue = 0.5F, ordinal = 0)
+    )
+    private float increaseRightArmSpeed(float constant) {
+        if (Config.BWOConfig.player.walkingAnim.toString().equals("Infdev") || Config.BWOConfig.player.walkingAnim.toString().equals("Classic")) {
+            return 1.0F;
+        }
+
+        return constant;
+    }
+
+    @ModifyConstant(
+            method = "setAngles",
+            constant = @Constant(floatValue = 0.5F, ordinal = 1)
+    )
+    private float increaseLeftArmSpeed(float constant) {
+        if (Config.BWOConfig.player.walkingAnim.toString().equals("Infdev") || Config.BWOConfig.player.walkingAnim.toString().equals("Classic")) {
+            return 1.0F;
+        }
+
+        return constant;
+    }
+
     @Inject(method = "setAngles", at = @At("TAIL"))
     private void playerWalkingAnimation(float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch, float scale, CallbackInfo ci) {
         if (Config.BWOConfig.player.walkingAnim.toString().equals("Infdev") || Config.BWOConfig.player.walkingAnim.toString().equals("Classic")) {
@@ -28,8 +54,8 @@ public class BipedEntityModelMixin extends EntityModel {
         }
 
         if (Config.BWOConfig.player.walkingAnim.toString().equals("Classic")) {
-            this.head.yaw = headYaw / 57.29578F + (float)Math.sin((double)(limbAngle * 0.6662F) * 0.23D) * limbDistance;
-            this.head.pitch = headPitch / 57.29578F + (float)Math.sin((double)(limbAngle * 0.6662F) * 0.1D) * 0.8F / 1.5F * limbDistance;
+            this.head.yaw = headYaw / 57.29578F + (float)Math.sin((double)(limbAngle * 1.3334F) * 0.23D) * limbDistance;
+            this.head.pitch = headPitch / 57.29578F + (float)Math.sin((double)(limbAngle * 1.3334F) * 0.1D) * 0.8F / 1.5F * limbDistance;
         }
     }
 
